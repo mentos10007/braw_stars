3d7424c57067fdf7969ef9b0cb8de65238db8dd7
mentos10007 <113818073+mentos10007@users.noreply.github.com>
2022-09-25 12:57:42 +0700

    это браво старс (не фейк)

---
+++ main.py
@@ -0,0 +1,61 @@
+# x = 637
+#
+# print("ПЕВАЯ цифра :", x % 10)
+# print("последняя цифра :", x // 100)
+
+# x = 123
+#
+# first = x // 100
+# second = (x // 10) % 10
+# tretie = x % 10
+# print(first, second, tretie )
+# print(f"сумма {x} =" , first+second+tretie)
+
+# print("введите стоиость одного пирожка :")
+#
+# rub = int(input("рублей:"))
+# cop = int(input("копеек:"))
+#
+# price_pie = rub * 100 + cop
+#
+# count = int(input("скоко пирожков:"))
+#
+# need_to_pay = count * price_pie
+# print(F"с вас  {need_to_pay // 100} рубле и  {need_to_pay % 100}  копеек")
+#
+# money = int(input("отдашь бабок:"))
+#
+# money_cop = money * 100
+# final_cdacha = money_cop - need_to_pay
+# print(f"сдача:{final_cdacha // 100} рублей и {final_cdacha % 100}" )
+#
+
+# x = int(input("число:"))
+#
+# first = x // 100
+# second = (x // 10) % 10
+# tretie = x % 10
+# print("произведение: ", first * second * tretie)
+#
+# x = int(input("число"))
+# if x % 2 == 1:
+#     x = x + 1
+# else:
+#     x = x + 2
+#
+# print(x)
+
+# d = int(input("диам6етр окружности:"))
+#
+# print("диаметр окружности", 3.14 * d)
+#
+# r = float(input("диам6етр окружности:"))
+#
+# print("диаметр окружности", 3.14 * r * r)
+#
+
+# n = int(input("часов  с начала дня:"))
+# h = n // 3600
+# m = n // 60
+# c = n % 60
+# print(f"прошло {h} часов {m} минут {c} секунд")
